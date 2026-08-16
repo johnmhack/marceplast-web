31b5b727dcea0f0ffece518616c1f53644f51ad6
johnmhack <62674550+johnmhack@users.noreply.github.com>
2026-08-15 19:37:36 -0500

    actualizamos el sitemap de la pagina para indexar en google

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://marceplast.com', // cambia esto por tu dominio real
+  site: 'https://www.marceplastsas.com',
 });
